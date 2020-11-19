7eca3c22344757c091a17ec1eb8b009a5817b18f
Matheus <thesirion@gmail.com>
2020-11-19 16:22:53 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+selectMediaStream();
